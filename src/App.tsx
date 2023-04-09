7595f4259d803f6d46d591a4afc68026a460aa4d
hoangkimduc <duc.hoang@employmenthero.com>
2023-04-09 15:48:08 +0700

    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ResponsiveAppBar from './components/navigation';
+import AuthProvider from './contexts/authProvider';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/homePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <ResponsiveAppBar/>
+
+        <Routes>
+          <Route index element={<HomePage />} />
+
+          {/* <Route path="*" element={<NoMatch />} /> */}
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
