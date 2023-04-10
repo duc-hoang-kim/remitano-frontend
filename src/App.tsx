09eb548a41ca0a1a26d66458b92281f04c9bbe46
hoangkimduc <duc.hoang@employmenthero.com>
2023-04-10 13:19:27 +0700

    apis for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
-import ResponsiveAppBar from "./components/navigation";
-import AuthProvider from "./contexts/authProvider";
+import ResponsiveAppBar from "./components/Navigation";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/homePage";
+import HomePage from "./pages/HomePage";
 import { Container } from "@mui/material";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import { AuthProvider } from "./contexts/AuthContext";
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
       <AuthProvider>
@@ -14,6 +16,8 @@ function App() {
         <Container sx={{ marginTop: "25px", marginBottom: "50px" }}>
           <Routes>
             <Route index element={<HomePage />} />
+            <Route path='/login' element={<LoginPage/>} />
+            <Route path='/register' element={<RegisterPage/>} />
 
             {/* <Route path="*" element={<NoMatch />} /> */}
           </Routes>
---
+++ src/features/authentication/hooks/useLogin.ts
@@ -0,0 +1,21 @@
+import { useAuthenticate } from "../../../contexts/AuthContext";
+import useFetch from "../../../hooks/useFetch";
+import { UserType } from "../types";
+
+const useLogin = () => {
+  const { logInAs } = useAuthenticate();
+
+  const { data, error, fetchApi } = useFetch({
+    path: "api/v1/users/sign_in",
+    method: "POST",
+    onSuccess: (res_body) => {
+      const user: UserType = { email: res_body.data.user.email };
+      logInAs(user);
+    }
+  });
+
+
+  return { data, error, fetchLogin: fetchApi };
+};
+
+export default useLogin;
